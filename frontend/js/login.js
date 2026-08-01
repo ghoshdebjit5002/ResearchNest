d40f1bbce4e52f4387c454f22da1f4ac458cebf2
ghoshdebjit5002 <ghoshdebjit5002@gmail.com>
2026-08-01 22:04:36 +0530

    Update login.js

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -3,7 +3,7 @@ document.getElementById("loginForm").addEventListener("submit", async e => {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   try {
-    const res = await fetch("http://localhost:4000/api/auth/login", {
+    const res = await fetch("/api/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password })
